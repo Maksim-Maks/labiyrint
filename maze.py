6d5786cf407a86dc1de5d5e1d7309c837aad342b
maks <test@mail.com>
2024-04-14 13:27:59 +0300

    added enemy moves and walls

--- maze.py
+++ maze.py
@@ -44,10 +44,46 @@ class Player(GameSprite):
 
 
 
+class Enemy(GameSprite):
+    direction = "left"
+    def update(self, x1 , x2):
+        if self.direction == 'left':
+            self.rect.x -= self.speed
+        elif self.direction == 'right':
+            self.rect.x += self.speed
+        
+        if self.rect.x <= x1:
+            self.direction = 'right'
+        elif self.rect.x >= x2:
+            self.direction = 'left'
+
+
+class Wall(pygame.sprite.Sprite):
+    def __init__(self,coords,size,color):
+        self.rect = pygame.rect.Rect(coords,size)
+        self.color = color
+    def draw_wall(self):
+        pygame.draw.rect(window,self.color, self.rect)
+
+
+
+
 
 player = Player("hero.png", (40,HEIGHT-40), 5)
-enemy = GameSprite("cyborg.png",(WIDTH-100,HEIGHT/2), 5)
+enemy = Enemy("cyborg.png",(WIDTH-100,HEIGHT/2), 5)
 gold = GameSprite("treasure.png",(WIDTH-40, HEIGHT-60), 0)
+
+
+walls = [ Wall((10,10),(WIDTH-100,10),(255,0,0)),
+Wall((10,10),(10,HEIGHT-150),(255,0,0)),
+Wall((10,HEIGHT-150),(200,10),(255,0,0)),
+Wall((300,HEIGHT-150),(700,10),(255,0,0)),
+Wall((495,HEIGHT-150),(10,500),(255,0,0)),
+Wall((495,100),(10,HEIGHT),(255,0,0)),
+Wall((595,100),(350,10),(255,0,0)),
+Wall((500,250),(300,10),(255,0,0)),
+Wall((100,95),(10,300),(255,0,0))
+]
 
 
 
@@ -61,8 +97,13 @@ while not game_over:
     window.blit(background,(0,0))
     player.update()
     player.reset()
+    enemy.update(WIDTH/2, WIDTH)
     enemy.reset()
     gold.reset()
+
+    for w in walls:
+        w.draw_wall()
+    
 
     pygame.display.update()
     clock.tick(FPS)
